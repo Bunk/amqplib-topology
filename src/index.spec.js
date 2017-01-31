bc86bfac1b1768a6219f19abe9d2d9a5c99fa713
JD Courtoy <jd.courtoy@gmail.com>
2017-01-31 15:19:45 -0600

    Added some test placeholders

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,39 @@
 /* eslint-env mocha */
-describe( "amqplib", () => {
+const { amqplib } = testHelpers;
+const factory = require( "./index" );
 
+describe( "amqplib", () => {
+	let lib, connection;
+
+	beforeEach( async () => {
+		connection = await amqplib.connect( "amqp://localhost" );
+		lib = factory( connection );
+	} );
+
+	context( "given a valid dsl", () => {
+		beforeEach( () => {
+			const dsl = {
+				exchanges: {},
+				queues: {},
+				bindings: {}
+			};
+			return lib.assert( dsl );
+		} );
+
+		it( "should assert all exchanges", () => {
+
+		} );
+
+		it( "should assert all queues", () => {
+
+		} );
+
+		it( "should assert all bindings", () => {
+
+		} );
+	} );
+
+	afterEach( () => {
+		amqplib.reset();
+	} );
 } );
